0d698a5a842d9395668c8fb64f8f718e6770f3a7
Callum <callum.egan@ericsson.com>
2026-08-07 14:00:58 +0100

    creature+enemy classes, hopping

--- Callum/FarmGame/code/player and bg.py
+++ Callum/FarmGame/code/player and bg.py
@@ -1,23 +1,21 @@
 import pygame
 from os.path import join
+import random
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self, groups):
+all_sprites = pygame.sprite.Group()
+
+class Creature(pygame.sprite.Sprite):
+    def __init__(self, groups,image):
         super().__init__(groups)
-        self.right_image = pygame.image.load("FarmGame/images/rabbit-pixilart.png").convert_alpha()
-        self.right_image = pygame.transform.scale_by(self.right_image,2)
+        self.right_image = image
+        #self.right_image = pygame.transform.scale_by(self.right_image,2)
         self.image = self.right_image
-        self.rect = self.image.get_frect(center = (WINDOW_WIDTH/2,WINDOW_HEIGHT/2))
         self.direction = pygame.math.Vector2()
-        self.speed = 600
         self.going_right = True
-    def update(self,dt):
-        keys = pygame.key.get_pressed()
-        self.direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
-        self.direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
-        self.direction = self.direction.normalize() if self.direction else self.direction
-        self.rect.center += self.direction * self.speed * dt
-        self.surf_direction()
+    #def update(self,dt):
+    #    self.direction = self.direction.normalize() if self.direction else self.direction
+    #    self.rect.center += self.direction * self.speed * dt
+    #    self.surf_direction()
     def surf_direction(self):
         if self.going_right:
             self.image = self.right_image
@@ -26,7 +24,48 @@ class Player(pygame.sprite.Sprite):
         if self.direction.x < 0:
             self.going_right = False
         elif self.direction.x > 0:
-            self.going_right = True
+            self.going_right = True  
+        self.mask = pygame.mask.from_surface(self.right_image)
+class Player(Creature):
+    def __init__(self, groups,image):
+        super().__init__(groups,image)
+        #self.right_image = pygame.image.load("FarmGame/images/rabbit-pixilart.png").convert_alpha()
+        self.right_image = pygame.transform.scale_by(self.right_image,2)
+        self.image = self.right_image
+        self.base_image = self. right_image
+        self.rect = self.image.get_frect(center = (WINDOW_WIDTH/2,WINDOW_HEIGHT/2))
+        #self.direction = pygame.math.Vector2()
+        self.speed = 600
+        #self.going_right = True
+        self.hop = False
+    def update(self,dt):
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
+        self.direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
+        self.direction = self.direction.normalize() if self.direction else self.direction
+        if pygame.key.get_just_pressed()[pygame.K_SPACE] and not self.hop:
+            self.hop = True
+            self.hop_start_time = pygame.time.get_ticks()
+            self.right_image =pygame.image.load("FarmGame/images/rabbit hop.png").convert_alpha()
+            self.right_image = pygame.transform.scale_by(self.right_image,2)
+        if self.hop == True:
+            self.speed = 900
+            if self.hop_start_time +500 <  pygame.time.get_ticks():
+                self.hop = False
+                self.right_image = self.base_image
+                self.speed = 300
+        self.rect.center += self.direction * self.speed * dt
+        self.surf_direction()
+
+    #def surf_direction(self):
+    #    if self.going_right:
+    #        self.image = self.right_image
+    #    else:
+    #        self.image = pygame.transform.flip(self.right_image,True,False)
+    #    if self.direction.x < 0:
+    #        self.going_right = False
+    #    elif self.direction.x > 0:
+    #        self.going_right = True
 
 class Background(pygame.sprite.Sprite): #makes backround and moves everything when player moves too far
     def __init__(self, surf, groups):
@@ -66,12 +105,33 @@ class YAwareGroup(pygame.sprite.Group): #draws sprites under or over others depe
             self.spritedict[spr] = surface_blit(spr.image, spr.rect)
         self.lostsprites = []
 
-class Enemies(pygame.sprite.Sprite): #placeholder replace with fully made enemy class
-    def __init__(self, groups):
-        super().__init__(groups)
-        self.image = pygame.image.load("Callum/FarmGame/images/Tree.png").convert_alpha()
-        self.image = pygame.transform.scale_by(self.image,1)
-        self.rect = self.image.get_frect(center = (WINDOW_WIDTH/2,WINDOW_HEIGHT/2))
+class Enemies(Creature): #placeholder replace with fully made enemy class
+    def __init__(self, groups,image,scale,pos):
+        super().__init__(groups,image)
+        #self.image = pygame.image.load("Callum/FarmGame/images/Tree.png").convert_alpha()
+        self.right_image = pygame.transform.scale_by(self.right_image,scale)
+        self.image = self.right_image
+        self.rect = self.image.get_frect(center = pos)
+        self.speed = 300
+    def update(self,dt):
+        self.direction.x = player.rect.centerx - self.rect.centerx
+        self.direction.y = player.rect.centery - self.rect.centery
+        if abs(self.direction.x) < 50:
+            self.direction.x =0
+        if abs(self.direction.y) < 50:
+            self.direction.y =0
+        self.direction = self.direction.normalize() if self.direction else self.direction
+        self.surf_direction()
+        self.anti_cramming()
+        self.rect.center += self.direction * self.speed * dt
+    def anti_cramming(self):
+        for sprite1 in creature_sprites:
+            for sprite2 in creature_sprites:
+                if sprite1 != sprite2:
+                    if pygame.sprite.collide_mask(sprite1, sprite2,):
+                        sprite1.rect.centerx += (sprite1.rect.centerx-sprite2.rect.centerx)**-1
+                        sprite1.rect.centery += (sprite1.rect.centery-sprite2.rect.centery)**-1
+
 
 
 pygame.init()
@@ -81,12 +141,14 @@ pygame.display.set_caption("Farm Game")
 clock = pygame.time.Clock()
 running = True
 
-background_surf = pygame.image.load("FarmGame/images/background.png").convert_alpha()
-all_sprites = pygame.sprite.Group()
-player = Player(all_sprites)
+creature_sprites = pygame.sprite.Group()
+background_surf = pygame.image.load("FarmGame/images/barn.png").convert_alpha()
+player = Player((all_sprites,creature_sprites),pygame.image.load("FarmGame/images/rabbit.png").convert_alpha())
 background = Background(background_surf,all_sprites) #replace background_surf with current level background
-placehold = Enemies(all_sprites)
-sprites = YAwareGroup((player,placehold)) # place any class of object into here EXCEPT background
+YAwareGroup(creature_sprites) # place any class of object into here EXCEPT background
+
+for x in range(3):
+    Enemies((all_sprites,creature_sprites),pygame.image.load("FarmGame/images/Angry Pig.png").convert_alpha(),5,(random.randint(0, WINDOW_WIDTH),random.randint(0, WINDOW_HEIGHT)))
 
 
 while running:
@@ -98,7 +160,7 @@ while running:
     display_surface.fill("#2FC66B")
     display_surface.blit(background.image,background.rect)
     background.move_bg(player,dt)
-    sprites.draw(display_surface)
+    creature_sprites.draw(display_surface)
     all_sprites.update(dt)
     pygame.display.update()
 pygame.quit()
